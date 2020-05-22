c3ebb0e406e7e77f1347e3ebf9f77f5498dd6174
Madoxen <pchlad5@gmail.com>
2020-05-22 15:06:19 +0200

    Added locale fix and started client project

--- MovieClassifier/Program.cs
+++ MovieClassifier/Program.cs
@@ -27,10 +27,10 @@ namespace MovieClassifier
             int[] hiddenNeurons = new int[args.Length - 4];
             for (int i = 4; i < args.Length; i++) hiddenNeurons[i - 4] = Convert.ToInt32(args[i]);
 
-           // Network net = new Network(ConvertUtil.ConvertArg(args[0]), ConvertUtil.ConvertArg(args[1]), 
-           //     ConvertUtil.ConvertArg(args[2]), ConvertUtil.ConvertArg(args[3]), 
-//imageWidth * imageHeight * imageDepth, hiddenNeurons, outputCount);
-            Network net = Network.LoadNetworkFromFile("record_weights_HighestHitTest_0,74");
+            Network net = new Network(ConvertUtil.ConvertArg(args[0]), ConvertUtil.ConvertArg(args[1]), 
+                ConvertUtil.ConvertArg(args[2]), ConvertUtil.ConvertArg(args[3]), 
+            imageWidth * imageHeight * imageDepth, hiddenNeurons, outputCount);
+            //Network net = Network.LoadNetworkFromFile("record_weights_HighestHitTest_0,74");
             net.testStrategy = new HighestHitTest(net);
 
             Console.WriteLine(" Loading data...");
