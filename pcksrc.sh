47aee6b68f84680b72f67e4a8336213e0ac936fb
Madoxen <pchlad5@gmail.com>
2020-05-25 16:14:46 +0200

    Upgraded util

--- pcksrc.sh
+++ pcksrc.sh
@@ -7,18 +7,23 @@ pyflist=$(find . | grep "\.py$")
 for f in $csflist 
 do
     echo -e "\n" >> completeSrc.txt
-    echo "============================" >> completeSrc.txt
-    echo $f >> completeSrc.txt
-    echo "============================" >> completeSrc.txt
+    filename=$(echo $f | grep -o '[^/]\+$')
+    echo "\subsubsection{$filename}" >> completeSrc.txt
+    echo "\begin{lstlisting}" >> completeSrc.txt
     cat $f >> completeSrc.txt
+    echo "\end{lstlisting}" >> completeSrc.txt
+    echo "\pagebreak" >> completeSrc.txt    
+  
 done
 
 
 for f in $pyflist 
 do
     echo -e "\n" >> completeSrc.txt
-    echo "============================" >> completeSrc.txt
-    echo $f >> completeSrc.txt
-    echo "============================" >> completeSrc.txt
+    filename=$(echo $f | grep -o '[^/]\+$')
+    echo "\subsubsection{$filename}" >> completeSrc.txt
+    echo "\begin{lstlisting}" >> completeSrc.txt
     cat $f >> completeSrc.txt
+    echo "\end{lstlisting}" >> completeSrc.txt
+    echo "\pagebreak" >> completeSrc.txt
 done
